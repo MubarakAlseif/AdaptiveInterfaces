973a5c5b63f2bdaacb58d86a35388b7ba64c14b8
Mobarak.Alseif@tuwaiq.edu.sa <Mobarak.Alseif@tuwaiq.edu.sa>
2021-11-03 10:22:05 +0300

    Done AdaptiveInterfaces

---
+++ AdaptiveInterfaces/Controllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  AdaptiveInterfaces
+//
+//  Created by Mobark Alseif on 28/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var viewCityName: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        viewCityName.layer.cornerRadius = 12
+    }
+
+
+}
+
